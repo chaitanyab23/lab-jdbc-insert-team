ac2ee4ad07e29994a952ea6ad6351d0b870a115e
chaitanyab23 <chaitanyabhagat23@gmail.com>
2020-09-09 13:44:11 +0530

    chaitanya

--- src/dao/CityDAO.java
+++ src/dao/CityDAO.java
@@ -1,1 +1,21 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import model.City;
+import utility.ConnectionManager;
+
+public class CityDAO{
+	public City getCityByName(String name) throws ClassNotFoundException, SQLException {
+		
+		Connection con= ConnectionManager.getConnection();
+		Statement st= con.createStatement();
+		ResultSet rs=st.executeQuery("SELECT * FROM PROKABBADI");
+		
+		return null;
+		
+	}
+}
